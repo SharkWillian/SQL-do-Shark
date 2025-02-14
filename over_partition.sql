f454b0a2f6657cc47879cca1da4bd796b798f282
Willian Reina Joviano <103294061+SharkWillian@users.noreply.github.com>
2025-02-14 11:47:40 -0300

    Create over_partition.sql

---
+++ over_partition.sql
@@ -0,0 +1,15 @@
+-- Para casos onde temos para o mesmo código, linhas com alguma informação divergente, gerando duplicidades.
+-- Exemplo: Mesmo código - registros em tempos diferentes (Daí vale observar o order by - vc quer o último ou primeiro registro?
+
+WITH CTE_RANKING AS (
+SELECT
+	A.*,
+ROW_NUMBER() OVER  (PARTITION BY 
+						CÓDIGO 
+					ORDER BY 
+						DATA_REF DESC) AS RANKING
+FROM TABELA A)
+SELECT *
+FROM CTE_RANKING 
+WHERE  
+	RANKING = 1) b ON a.CÓDIGO = b.CÓDIGO
