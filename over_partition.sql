1d0dd3d379ca3a78b370c5b8deda0602969924cf
Willian Reina Joviano <103294061+SharkWillian@users.noreply.github.com>
2025-03-26 10:01:17 -0300

    Update over_partition.sql

--- over_partition.sql
+++ over_partition.sql
@@ -2,6 +2,17 @@
 -- Exemplo: Mesmo código - registros em tempos diferentes (Daí vale observar o order by - vc quer o último ou primeiro registro?
 
 WITH CTE_RANKING AS (
+    SELECT
+        A.*,
+        ROW_NUMBER() OVER (PARTITION BY CÓDIGO ORDER BY DATA_REF DESC) AS RANKING
+    FROM TABELA A
+)
+SELECT *
+FROM CTE_RANKING 
+WHERE RANKING = 1;
+
+
+WITH CTE_RANKING AS (
 SELECT
 	A.*,
 ROW_NUMBER() OVER  (PARTITION BY 
